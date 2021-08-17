bdfb66163614c3d3210137071771cb26291f93ab
George Doenlen <gdoenlen@pinnacle21.com>
2021-08-17 12:19:12 -0400

    further optimize c++

--- fishers-exact-test/c++/cppfet.cpp
+++ fishers-exact-test/c++/cppfet.cpp
@@ -34,19 +34,14 @@ double logHypergeometricProbability(const std::array<long, 4>& data, const std::
 	);
 }
 
-double fisher_exact(const std::array<long, 4>& data, std::array<double, FACTORIAL_SIZE>& factorials) {
-    // calculate our rejection threshold
-	const double pvalThreshold = logHypergeometricProbability(data, factorials);
-	
+double fisher_exact(const std::array<long, 4>& data, const std::array<double, FACTORIAL_SIZE>& factorials, const double pvalThreshold) {
     double pvalFraction = 0;
 	for(long i = 0; i <= GRAND_TOTAL; i++) {
-		if((data[0] + data[1] - i >= 0) && (data[0] + data[2] - i >= 0) && (data[3] - data[0] + i >=0)) {
-            double lhgp = logHypergeometricProbability({
-                i,
-                data[0] + data[1] - i,
-                data[0] + data[2] - i,
-                data[3] - data[0] + i
-            }, factorials);
+        long one = data[0] + data[1] - i;
+        long two = data[0] + data[2] - i;
+        long three = data[3] - data[0] + i;
+		if(one >= 0 && two >= 0 && three >= 0) {
+            double lhgp = logHypergeometricProbability({ i, one, two, three }, factorials);
 
 			if(lhgp <= pvalThreshold) {
 				pvalFraction += exp(lhgp - pvalThreshold);
@@ -66,8 +61,9 @@ int main(int argc, char* argv[]) {
     
     double pvalue = 0.0;
     std::array<double, FACTORIAL_SIZE> factorials = logFactorial();
+	const double pvalThreshold = logHypergeometricProbability(DATA, factorials);
 	for(size_t i = 0; i < atoi(argv[1]); i++) {
-		pvalue = fisher_exact(DATA, factorials);
+		pvalue = fisher_exact(DATA, factorials, pvalThreshold);
 	}
     
     std::cout << "pvalue = " << pvalue << "\n";
